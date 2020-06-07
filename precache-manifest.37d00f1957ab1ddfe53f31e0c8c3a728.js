89261477f66e011bcf2e095260eaf2950534bf36
Anton <diamonddiver99@gmail.com>
2020-06-07 15:20:44 +0300

    edit2

---
+++ precache-manifest.37d00f1957ab1ddfe53f31e0c8c3a728.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17b346e9252d240e47333f336cb806ce",
+    "url": "/React-graduate-work/index.html"
+  },
+  {
+    "revision": "365e4b14f39e7cac54e7",
+    "url": "/React-graduate-work/static/css/main.31dc98e9.chunk.css"
+  },
+  {
+    "revision": "d41bca9b37705daae795",
+    "url": "/React-graduate-work/static/js/2.09e93c1b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/React-graduate-work/static/js/2.09e93c1b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "365e4b14f39e7cac54e7",
+    "url": "/React-graduate-work/static/js/main.cc31d64d.chunk.js"
+  },
+  {
+    "revision": "e5a19523ce11ff585e6a",
+    "url": "/React-graduate-work/static/js/runtime-main.236f5faf.js"
+  }
+]);
